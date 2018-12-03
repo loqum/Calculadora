0b675e0a6f88c44c13ba63bf1d4ab796ce4703c1
loqum <ruben693@gmail.com>
2018-12-03 17:26:18 +0100

    Añadido eventos a los botones

--- app/src/main/java/com/example/rfm/calculadora/MainActivity.java
+++ app/src/main/java/com/example/rfm/calculadora/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rfm.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
             buttonOcho, buttonSiete, buttonSeis, buttonCinco, buttonCuatro, buttonTres, buttonDos,
             buttonUno, buttonCero, buttonBorrar, buttonDecimal;
     TextView etiqueta;
+    double resultado;
+    String operador, mostrar, reserva;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,96 @@ public class MainActivity extends AppCompatActivity {
         buttonDecimal = findViewById(R.id.decimal);
 
         etiqueta = findViewById(R.id.etiquetaResultado);
+
+        buttonNueve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrar = etiqueta.getText().toString();
+                mostrar = mostrar + "9";
+                etiqueta.setText(mostrar);
+            }
+        });
+
+        buttonOcho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrar = etiqueta.getText().toString();
+                mostrar = mostrar + "8";
+                etiqueta.setText(mostrar);
+            }
+        });
+
+        buttonSiete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrar = etiqueta.getText().toString();
+                mostrar = mostrar + "7";
+                etiqueta.setText(mostrar);
+            }
+        });
+
+        buttonSeis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrar = etiqueta.getText().toString();
+                mostrar = mostrar + "6";
+                etiqueta.setText(mostrar);
+            }
+        });
+
+        buttonCinco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrar = etiqueta.getText().toString();
+                mostrar = mostrar + "5";
+                etiqueta.setText(mostrar);
+            }
+        });
+
+        buttonCuatro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrar = etiqueta.getText().toString();
+                mostrar = mostrar + "4";
+                etiqueta.setText(mostrar);
+            }
+        });
+
+        buttonTres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrar = etiqueta.getText().toString();
+                mostrar = mostrar + "3";
+                etiqueta.setText(mostrar);
+            }
+        });
+
+        buttonDos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrar = etiqueta.getText().toString();
+                mostrar = mostrar + "2";
+                etiqueta.setText(mostrar);
+            }
+        });
+
+        buttonUno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrar = etiqueta.getText().toString();
+                mostrar = mostrar + "1";
+                etiqueta.setText(mostrar);
+            }
+        });
+
+        buttonCero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrar = etiqueta.getText().toString();
+                mostrar = mostrar + "0";
+                etiqueta.setText(mostrar);
+            }
+        });
 
 
     }
